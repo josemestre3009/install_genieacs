680af3d49570413e817708c1d472b1b0eddacb72
josemestre3009 <mestresaucedo@gmail.com>
2025-06-04 09:27:25 -0500

    SE ACTUALIZO MONGO DB

--- install_genieacs.sh
+++ install_genieacs.sh
@@ -21,8 +21,16 @@ npm -v
 
 # Instalar MongoDB
 echo "Instalando MongoDB..."
-sudo apt update
-sudo apt install -y mongodb
+sudo apt-get install gnupg curl
+curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
+   --dearmor
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu focal/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
+
+sudo apt-get update
+sudo apt-get install -y mongodb-org
+sudo systemctl start mongod
+
 
 # Verificar que MongoDB está corriendo
 echo "Verificando estado de MongoDB..."
